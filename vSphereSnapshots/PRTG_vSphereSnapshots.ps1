0742baf976afe8a3c6c7b6a8c4ac965d4c10e0e6
Rene Gessinger <rene.gessinger@concat.de>
2022-03-22 13:44:05 +0100

    fix indent

--- vSphereSnapshots/PRTG_vSphereSnapshots.ps1
+++ vSphereSnapshots/PRTG_vSphereSnapshots.ps1
@@ -1,18 +1,18 @@
 [CmdletBinding()] param(
     [Parameter(Mandatory=$true)]
-	[string]$server = $null,
+    [string]$server = $null,
     [Parameter(Mandatory=$true)]
-	[string]$user = $null,
-	[Parameter(Mandatory=$true)]
-	[string]$password = $null,
-	[Parameter(ParameterSetName="setAge", Mandatory=$true)]
-	[AllowNull()]
+    [string]$user = $null,
+    [Parameter(Mandatory=$true)]
+    [string]$password = $null,
+    [Parameter(ParameterSetName="setAge", Mandatory=$true)]
+    [AllowNull()]
     [Nullable[System.Int32]]$maxAge,
-	[Parameter(ParameterSetName="setSize", Mandatory=$true)]
-	[AllowNull()]
+    [Parameter(ParameterSetName="setSize", Mandatory=$true)]
+    [AllowNull()]
     [Nullable[System.Int32]]$maxMb,
-	[Parameter(Mandatory=$false)]
-	[switch]$allLinked = $false
+    [Parameter(Mandatory=$false)]
+    [switch]$allLinked = $false
 )
 
 Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -Confirm:$false >$null 2>&1
@@ -24,35 +24,35 @@ $snapvms = Get-VM -Tag $tags
 $vms = Get-VM | Where-Object { $snapvms -notcontains $_ }
 $message = ""
 if($null -ne $maxAge) {
-	$message = "Snapshots older than $($maxAge) hours"
+    $message = "Snapshots older than $($maxAge) hours"
 } elseif($null -ne $maxMb) {
-	$message = "Snapshots bigger than $($maxMb) MB"
+    $message = "Snapshots bigger than $($maxMb) MB"
 }
 foreach ($snap in $vms | Get-Snapshot)
 {
-	$snapshot = New-Object PSCustomObject
-	$snapevent = Get-VIEvent -Entity $snap.VM -Types Info -Finish $snap.Created -MaxSamples 1 | Where-Object { $_.FullFormattedMessage -imatch 'Task: Create virtual machine snapshot' }
-	$snapshot | Add-Member -type NoteProperty -name VM $snap.VM	
-	$snapshot | Add-Member -type NoteProperty -name Name $snap.Name
-	$snapshot | Add-Member -type NoteProperty -name SizeMB $snap.sizemb
-	$snapshot | Add-Member -type NoteProperty -name created $snap.created
-	if ($null -ne $snapevent) {
-		$snapshot | Add-Member -type NoteProperty -name CreatedBy $snapevent.UserName
-	}
-	else {
-		$snapshot | Add-Member -type NoteProperty -name CreatedBy "UnKnown"
-	}
-	if($null -ne $maxAge) {
-		if($snapshot.created -lt (Get-Date).AddHours(($maxDays * -1))) {
-			$data += $snapshot
-		}
-	} elseif($null -ne $maxMb) {
-		if($snapshot.SizeMB -gt $maxMb) {
-			$data += $snapshot
-		}
-	} else {
-		throw "Missing parameter maxAge or maxMb"
-	}
+    $snapshot = New-Object PSCustomObject
+    $snapevent = Get-VIEvent -Entity $snap.VM -Types Info -Finish $snap.Created -MaxSamples 1 | Where-Object { $_.FullFormattedMessage -imatch 'Task: Create virtual machine snapshot' }
+    $snapshot | Add-Member -type NoteProperty -name VM $snap.VM    
+    $snapshot | Add-Member -type NoteProperty -name Name $snap.Name
+    $snapshot | Add-Member -type NoteProperty -name SizeMB $snap.sizemb
+    $snapshot | Add-Member -type NoteProperty -name created $snap.created
+    if ($null -ne $snapevent) {
+        $snapshot | Add-Member -type NoteProperty -name CreatedBy $snapevent.UserName
+    }
+    else {
+        $snapshot | Add-Member -type NoteProperty -name CreatedBy "UnKnown"
+    }
+    if($null -ne $maxAge) {
+        if($snapshot.created -lt (Get-Date).AddHours(($maxDays * -1))) {
+            $data += $snapshot
+        }
+    } elseif($null -ne $maxMb) {
+        if($snapshot.SizeMB -gt $maxMb) {
+            $data += $snapshot
+        }
+    } else {
+        throw "Missing parameter maxAge or maxMb"
+    }
 }
 
 $count = $data | Measure-Object | Select-Object -expand Count
